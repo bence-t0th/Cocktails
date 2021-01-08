88eb36029b7ba6af5103742375cd65febefae261
bence-t0th <bence.toth.edu@icloud.com>
2021-01-08 19:30:44 +0100

    Icon and resources added

---
+++ Cocktails/Cocktails/CocktailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailsApp.swift
+//  Cocktails
+//
+//  Created by Bence Tóth on 2021. 01. 08..
+//
+
+import SwiftUI
+
+@main
+struct CocktailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
